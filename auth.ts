fc7156ac528748e1a6c3e525819bd0c5ede5607c
NazimRiyadh <neariyadh786@gmail.com>
2025-11-03 02:21:33 +0600

    Google authetication added

--- auth.ts
+++ auth.ts
@@ -1,5 +1,6 @@
-import NextAuth from "next-auth"
-import GitHub from "next-auth/providers/github"
+import NextAuth from "next-auth";
+import GitHub from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
@@ -7,5 +8,9 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       clientId: process.env.AUTH_GITHUB_ID || "",
       clientSecret: process.env.AUTH_GITHUB_SECRET || "",
     }),
+    Google({
+      clientId: process.env.AUTH_GOOGLE_ID || "",
+      clientSecret: process.env.AUTH_GOOGLE_SECRET || "",
+    }),
   ],
-})
+});
